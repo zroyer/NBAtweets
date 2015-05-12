18b4e2aa35d114c1611debc2c021ec480ce92789
Zach Royer <zroyer@gmail.com>
2015-05-12 15:51:01 -0500

    server build up

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
 var express = require('express')
+var exphbs = require('express-handlebars')
+var http = require('http')
+var mongoose = require('mongoose')
+var twitter = require('ntwitter')
+var routes = require('./routes')
+var config = require('./config')
+var streamHandler = require('./utils/streamHandler')
+
+var app = express();
+var port = process.env.PORT || 4040
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.disable('etag')
+
+mongoose.connect('mongodb://localhost/nba-tweets')
+
+var twit = new twitter(config.twitter)
+
+app.get('/', routes.index)
+app.get('/page/:page/:skip', routes.page)
+
+app.use("/", express.static(__dirname + "/public/"))
+
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+})
+
+var io = require('socket.io').listen(server)
+
+
+
+
+
+twit.stream('statuses/filter',{ track: '#NBAPlayoffs'}, function(stream){
+  streamHandler(stream,io)
+})
+
+
