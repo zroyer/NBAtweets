5b9e5b04b7e2ed7eba337d5f69354091771d8aa6
Zach Royer <zroyer@gmail.com>
2015-05-12 21:51:13 -0500

    model created

---
+++ models/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'sEgZxxi0MPTOgSHj0yDzhKQHI',
+    consumer_secret: 'LNWtqu1VkrK62gI66V7NqvvnmiDaCxHWVq3koLMhB3vkvXBn6r',
+    access_token_key: '3193620128-2c8990A2nmInh4xliuanM5G9aFEnTfUZj6DAqKj',
+    access_token_secret: '3spzVIDvBh33hjtldE3TLtAWrbQLKic5qA6eN0mw4MnNi'
+  }
+}
---
+++ models/tweet.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose')
+
+
+var schema = new mongoose.Schema({
+    twid: String,
+    active: Boolean,
+    author: String,
+    avatar: String,
+    body: String,
+    date: Date,
+    screenname: String
+});
+
+
+schema.statics.getTweets = function(current, offset, cb) {
+  var tweets = []
+  var start = (current*10) + (offset * 1);
+
+  Tweet.find({},'twid active author avatar body date screenname',{offset: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs){
+    if(!err) {
+      tweets = docs
+      tweets.forEach(function(tweet){
+        tweet.active = true
+      });
+    } 
+    cb(tweets)
+  });
+};
+
+module.exports = Tweet = mongoose.model('Tweet', schema)
