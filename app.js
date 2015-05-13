a8a9b63bb572d914b6bfc0ac59efc975d98bb7ae
Zach Royer <zroyer@gmail.com>
2015-05-12 23:10:50 -0500

    app.js and procfile

---
+++ app.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var TweetsApp = require('./components/TweetsApp.react');
+var initialState = JSON.parse(document.getElementById('initial-state').innerHTML)
+
+React.renderComponent(
+  <TweetsApp tweets={initialState}/>,
+  document.getElementById('react-app')
+);
